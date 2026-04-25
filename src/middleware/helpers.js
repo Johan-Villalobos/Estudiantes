71afbd0cf33d0cf6930c44e13f093766e2c1225d
Johan Villalobos <johanvp74123@gmail.com>
2026-04-24 23:28:16 -0500

    Swagger

--- src/middleware/helpers.js
+++ src/middleware/helpers.js
@@ -22,11 +22,9 @@ function jsonBody(req) {
  */
 function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
-  res.setHeader("Access-Control-Max-Age", "86400");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 }
-
 /**
  * Envía una respuesta JSON (incluye CORS para garantizar que siempre se envíen)
  */
